e06032e07bd5b0e2cd4ac5aec5831f96a1ed219b
Utkarsh-Subhedar <utkarshsubhedar2017@gmail.com>
2026-01-03 22:24:12 +0530

    Add:Signup page designed

--- src/pages/login-signup/SignUp.jsx
+++ src/pages/login-signup/SignUp.jsx
@@ -7,47 +7,62 @@ const SignUp = () => {
   const [UserData, setUserData] = useState({
     Username: "",
     Password: "",
+    email: "",
   });
   const Navigate = useNavigate();
   const handleChange = (e) => {
+    const { name, value } = e.target;
     setUserData((prev) => ({
       ...prev,
-      [e.target.name]: e.target.value,
+      [name]: value,
     }));
   };
 
   const SaveUserData = () => {
-    console.log("User", UserData.Username);
-    if (!UserData.Username || !UserData.Password) {
-      alert("Both Fields are required");
-    } else {
-      localStorage.setItem("UserData", JSON.stringify(UserData));
-      Navigate("/Login");
-    }
+    // console.log("User", UserData.Username);
+    // if (!UserData.Username || !UserData.Password) {
+    //   alert("Both Fields are required");
+    // } else {
+    //   localStorage.setItem("UserData", JSON.stringify(UserData));
+    //   Navigate("/Login");
+    // }
   };
   return (
-    <div className="m-[6rem]">
-      <div className="text-center font-roboto text-slate-200 mb-8 text-3xl">
+    <div className="m-[2rem]">
+      <div className="text-center font-roboto font-semibold text-slate-200 mb-8 text-2xl md:text-3xl">
         NEW USER SIGNUP{" "}
       </div>
       <div className="flex justify-center">
         <div className="bg-slate-300 rounded-md *:text-black p-10 space-y-3 w-[20rem] ">
           <div className="*:font-roboto">
-            <label className="text-lg">Enter Username</label>
+            <label className="text-base md:text-lg">Enter Username</label>
             <Input
               type="text"
               className="w-[14rem] text-slate-200"
               name="Username"
-              Placeholder="Enter Username"
+              Placeholder="Enter First & Last Name"
+              value={UserData.Username}
               onChange={handleChange}
             />
           </div>
           <div className="*:font-roboto">
-            <label className="text-lg">Enter Password</label>
+            <label className="text-base md:text-lg">Enter Email</label>
+            <Input
+              type="email"
+              className="w-[14rem] text-slate-200"
+              name="email"
+              Placeholder="Enter Email"
+              value={UserData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="*:font-roboto">
+            <label className="text-base md:text-lg">Enter Password</label>
             <Input
               type="text"
               className="w-[14rem] text-slate-200"
               name="Password"
+              value={UserData.Password}
               Placeholder="Enter Password"
               onChange={handleChange}
             />
