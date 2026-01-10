f6d51193f7a2be086b9a3ea3eec71c9ea1c2eb4b
Utkarsh-Subhedar <utkarshsubhedar2017@gmail.com>
2026-01-10 22:49:05 +0530

    Update minor ui bug resolved in layout page

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
     <div>
       <Navbar />
       {/* Add top padding equal to Navbar height */}
-      <main className="pt-16 sm:pt-[4rem] md:pt-[4rem]">
+      <main className="pt-16 sm:pt-[4rem] md:pt-[4rem] pb-10">
         <Outlet />
       </main>
       <Footer />
