f7db1f11e8ed66783346af52cbb4ec692a5d8095
Utkarsh-megsys <utkarsh.s@megsys.digital>
2026-01-16 23:26:49 +0530

    Update:User profile functionality chnaged

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import ProfileDropdown from "./ProfileDropdown";
 import { MdAccountCircle } from "react-icons/md";
 
-const UserProfile = ({ profile, userData }) => {
+const UserProfile = ({ profile }) => {
   const [open, setOpen] = useState(false);
-
+  const userData = JSON.parse(localStorage.getItem("userCredentials"));
   return (
     <div className="h-auto sm:max-h-[50px] max-w-[200px] ">
       <div
@@ -12,7 +12,7 @@ const UserProfile = ({ profile, userData }) => {
         onClick={() => setOpen(!open)}
       >
         <div className="flex items-center dark:bg-gray-900 bg-gray-200 hover:bg-zinc-300 max-w-[150px] rounded-full">
-          <ProfileDropdown userData={userData} open={open} setOpen={setOpen} />
+          <ProfileDropdown name={userData.name} open={open} setOpen={setOpen} />
           <MdAccountCircle
             className="dark:text-gray-200
             text-black text-4xl"
